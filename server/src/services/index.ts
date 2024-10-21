2ff2e3f41a1bed893d676b83790fbf4f5c0867bd
JakubVavra <69152298+JakubVavra@users.noreply.github.com>
2024-10-21 16:46:18 +0200

    Init

---
+++ server/src/services/index.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+import actionService from './action-service';
+import emitService from './emit-service';
+import publicationService from './publication-service';
+import settingsService from './settings-service';
+
+export default {
+    action: actionService,
+    emitService,
+    publicationService,
+    settingsService,
+};
